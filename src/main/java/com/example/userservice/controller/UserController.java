2145737e33becc338cde303f76e5069546fd014d
byeungoo <dksekfldks65@naver.com>
2021-08-07 16:35:06 +0900

    user service

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -8,23 +8,25 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/user-service")
 public class UserController {
 
     private final UserService userService;
+    private final Environment environment;
 
     @Value("${greeting.message}")
     private String greeting;
 
     @GetMapping("/health_check")
     public String status() {
-        return "It's Working in User Service";
+        return String.format("It's Working in User Service on Port %s", environment.getProperty("local.server.port"));
     }
 
     @GetMapping("/welcome")
