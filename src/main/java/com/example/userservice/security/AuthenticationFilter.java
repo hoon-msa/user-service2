b2f092934568c9cd56a0949621f33abec68ad04c
byeungoo <dksekfldks65@naver.com>
2021-08-08 17:16:10 +0900

    user service

---
+++ src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.example.userservice.security;
+
+import com.example.userservice.vo.RequestLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+
+        try {
+            //post 형태로 body에 오기 때문에 inputStream으로 받는다.
+            RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
+
+            //AuthenticationManager에서 인증 처리 해줌
+            return getAuthenticationManager().authenticate(
+                    //마지막 인자는 어떤 권한을 갖을 것인지 전달
+                    new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>())
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+    }
+
+}
--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,13 +1,25 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment env;
 
     /**
      * 권한 작업
@@ -17,8 +29,32 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        //http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+            .hasIpAddress("192.168.27.17") //IP 제한
+            .and()
+            .addFilter(getAuthentionFilter());
+
         http.headers().frameOptions().disable();
     }
 
+    /*
+     * 인증 처리
+     */
+    private AuthenticationFilter getAuthentionFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+        return authenticationFilter;
+    }
+
+    /**
+     * 권한 처리
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //사용자가 전달한 유저 이메일과 비밀번호로 사용자 정보 조회;
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
 }
